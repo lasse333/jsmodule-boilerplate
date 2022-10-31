0e4b393aa27e072dbdb148437a8e3d4ddf5102cf
lasse333 <reneskjodt333@gmail.com>
2022-10-31 12:12:47 +0100

    Update FastHTML.js

--- assets/js/FastHTML.js
+++ assets/js/FastHTML.js
@@ -4,17 +4,26 @@ export function openFullscreen(element) {
     } else if (element.webkitRequestFullscreen) { /* Safari */
         element.webkitRequestFullscreen();
     } else if (element.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
+        element.msRequestFullscreen();
     }
 }
 
+/**
+ * 
+ * @param {string} type 
+ * @param {Object} [attributesIn] 
+ * @param {(HTMLElement|string|DocumentFragment)[]} [childrenIn] 
+ * @returns {HTMLElement}
+ */
 export function createElement(type, attributesIn, childrenIn) {
     let attributes = attributesIn ?? {}
     let children = childrenIn ?? []
-
+    
     let element = document.createElement(type)
     Object.assign(element, attributes)
-
+    if (attributes.class) element.className = attributes.class
+    if (attributes.for) element.setAttribute("for", attributes.for)
+    
     if (attributes.style) changeStyle(element, attributes.style)
     
     addChildren(element, children) 
@@ -22,9 +31,15 @@ export function createElement(type, attributesIn, childrenIn) {
     return element
 }
 
+/**
+ * 
+ * @param {string|HTMLElement} parrentElement 
+ * @param {Object|CSSStyleSheet} style 
+ * @returns {HTMLElement|undefined}
+ */
 export function changeStyle(parrentElement, style) {
-    if (typeof parrentElement == "String") {
-        document.querySelectorAll(selectors).forEach((element) => {
+    if (typeof parrentElement === "string") {
+        document.querySelectorAll(parrentElement).forEach((element) => {
             applyStyle(element, style)
         })
     } else {
@@ -33,45 +48,102 @@ export function changeStyle(parrentElement, style) {
     }
 }
 
+/**
+ * @param {HTMLElement} element 
+ * @param {Object|CSSStyleSheet} style 
+ */
 function applyStyle(element, style) {
     if (!(style instanceof CSSStyleSheet)) Object.assign(element.style, style)
     else {
-            if (!element.shadowRoot) element.attachShadow({mode: "open"})
-            element.shadowRoot.adoptedStyleSheets = [...document.adoptedStyleSheets, style]
+        if (!element.shadowRoot) element.attachShadow({mode: "open"})
+        element.shadowRoot.adoptedStyleSheets = [...document.adoptedStyleSheets, style]
     }
 }
 
-export function addChildren(element, children) {
+/**
+ * @param {HTMLElement} element 
+ * @param {(HTMLElement|string|DocumentFragment)[]|string|HTMLElement|DocumentFragment} childrenIn 
+ * @returns {HTMLElement}
+ */
+export function addChildren(element, childrenIn) {
+    let children = childrenIn
+    if (!(children instanceof Array)) children = [children]
     children.forEach(childIn => {
         let child = childIn
         let parser = new DOMParser()
-
-        if (typeof child == "string") {
+        
+        if (typeof child === "string") {
             child = parser.parseFromString(child, "text/html")
             child.querySelectorAll("script").forEach(scriptTag => scriptTag.remove())
             addChildren(element, Array.from(child.body.children))
             return element
         }
-
-        if (element.shadowRoot) element.shadowRoot.appendChild(child)
-        else element.appendChild(child)
+        
+        if (element.shadowRoot) element.shadowRoot.append(child)
+        else element.append(child)
     })
-
+    
     return element
 }
 
+/**@param {string} path */
 export function changeUrlPath(path) {
-
+    
+    const reRenderEvent = new CustomEvent("rerender", {detail: path})
+    window.dispatchEvent(reRenderEvent)
     window.history.pushState("", "", path);
-
+    
 }
 
+export function reRender() {
+    const reRenderEvent = new CustomEvent("rerender", {detail: ""})
+    window.dispatchEvent(reRenderEvent)
+}
+
+/** 
+ * @param {HTMLElement | string} parrentElement
+ * @returns {undefined | HTMLElement}
+ *  */
 export function clearChildren(parrentElement) {
-    if (typeof parrentElement == "String") {
+    
+    if (typeof parrentElement === "string") {
         document.querySelectorAll(parrentElement).forEach((element) => {
             element.innerHTML = ""
         })
     } else {
         parrentElement.innerHTML = ""
+        return parrentElement
+    }
+
+}
+
+function urlParams(params) {
+    let urlParams = {}
+    for (let name in params) {
+        if (params[name] == undefined) continue
+        for (let value in params[name].groups) {
+            if (value == "0") continue
+            urlParams[value] = params[name].groups[value]
+        }
+    }
+
+    return urlParams
+}
+
+
+export function simpleRoutes(routes={}) {
+    for (let path in routes) {
+        let url = new URLPattern({pathname: path})
+
+        if (url.test(window.location.href)) return routes[path](urlParams(url.exec(window.location.href)))
+    }
+}
+
+
+export async function simpleRoutesAsync(routes={}) {
+    for (let path in routes) {
+        let url = new URLPattern({pathname: path})
+
+        if (url.test(window.location.href)) return await routes[path](urlParams(url.exec(window.location.href)))
     }
 }
