9296d79de75e06c38ab0e2ecdb9e41d258a1fb56
lasse333 <reneskjodt333@gmail.com>
2022-10-31 12:13:30 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,21 @@
 import App from './App.js';
+import { clearChildren } from './FastHTML.js';
 import mainStyle from "../css/style.css" assert {type: "css"}
-// **  IIFE: Immediately Invoked Function Expression  */
 
+// **  IIFE: Immediately Invoked Function Expression  */
 (function () {
 
     document.adoptedStyleSheets = [mainStyle]
 
     App();
+
+    window.addEventListener('rerender', function (e) {
+        if (e.detail != window.location.pathname) {
+            clearChildren(document.body)
+            App()
+        }
+        
+    });
    
 
 
