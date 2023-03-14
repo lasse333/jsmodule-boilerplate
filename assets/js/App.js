fa7ca516ae932b800e098936a3ef9ecbd9272dbc
Lasse333 <reneskjodt333@gmail.com>
2023-03-14 21:59:13 +0000

    Update 2 files

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,23 +1,36 @@
 import { addChildren, createElement } from "./FastHTML.js";
 import IndexedDB from "./components/IndexedDB.js";
+import Camera, { BarcodeScanner } from "./components/Camera.js";
 
 async function App() {
-  let fridge = new IndexedDB("fridge", [
-    { storeName: "items", dataType: "object" },
-    { storeName: "images", dataType: "arrayBuffer" },
-  ]);
-  await fridge.open();
+  let camera = new Camera({
+    video: { height: 200, width: 200, facingMode: "environment" },
+  });
+  let scanner = new BarcodeScanner(camera.videoElement, "ean_13");
+  let codeOutput = createElement("p");
+  scanner.onScan = (code) => {
+    codeOutput.innerText = code;
+    scanner.stop();
+    camera.stopCamera();
+  };
+  {
+    let fridge = new IndexedDB("fridge", [
+      { storeName: "items", dataType: "object" },
+      { storeName: "images", dataType: "arrayBuffer" },
+    ]);
+    await fridge.open();
 
-  const { items, images } = fridge.tables;
+    const { items, images } = fridge.tables;
 
-  let buffer = new Uint8Array([1, 2, 3]).buffer;
+    let buffer = new Uint8Array([1, 2, 3]).buffer;
 
-  let myImage = await images.add(buffer);
-  await items.add({ image: myImage, name: "hello" });
-  console.log(await myImage);
-  console.log(await images.get(myImage));
+    let myImage = await images.add(buffer);
+    await items.add({ image: myImage, name: "hello" });
+    console.log(await myImage);
+    console.log(await images.get(myImage));
 
-  console.log(fridge);
+    console.log(fridge);
+  }
 
   let header;
   let main;
@@ -25,8 +38,22 @@ async function App() {
 
   addChildren(document.body, [
     (header = createElement("header")),
-    (main = createElement("main")),
-    (footer = createElement("footer")),
+    (main = createElement("main", {}, [camera.videoElement, codeOutput])),
+    (footer = createElement("footer", {}, [
+      createElement("button", {
+        onclick: async function () {
+          await camera.startCamera();
+        },
+        innerText: "startCamera",
+      }),
+      createElement("button", {
+        onclick: async function () {
+          if (!camera.running) await camera.startCamera();
+          await scanner.start();
+        },
+        innerText: "startScanner",
+      }),
+    ])),
   ]);
 }
 
---
+++ assets/js/components/Camera.js
@@ -0,0 +1,110 @@
+export default class Camera {
+  constructor(
+    constraints = { video: true },
+    videoElement = document.createElement("video")
+  ) {
+    this.videoElement = videoElement;
+    this.constraints = constraints;
+    this.recorder = null;
+    this.running = false;
+    this.onFrame = null;
+  }
+
+  async startCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+    this.videoElement.srcObject = stream;
+    await this.videoElement.play();
+    const canvas = document.createElement("canvas");
+    canvas.width = this.videoElement.videoWidth;
+    canvas.height = this.videoElement.videoHeight;
+    const context = canvas.getContext("2d");
+    this.recorder = new MediaRecorder(stream);
+    this.recorder.ondataavailable = (event) => {
+      if (this.running) {
+        context.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
+        this.onFrame(canvas.toDataURL("image/jpeg"));
+      }
+    };
+    this.recorder.start();
+    this.running = true;
+  }
+
+  stopCamera() {
+    this.running = false;
+    if (this.recorder && this.recorder.state !== "inactive") {
+      this.recorder.stop();
+      this.recorder = null;
+    }
+    if (this.videoElement.srcObject) {
+      const tracks = this.videoElement.srcObject.getTracks();
+      tracks.forEach((track) => track.stop());
+      this.videoElement.srcObject = null;
+    }
+  }
+
+  async takePicture() {
+    const canvas = document.createElement("canvas");
+    canvas.width = this.videoElement.videoWidth;
+    canvas.height = this.videoElement.videoHeight;
+    const context = canvas.getContext("2d");
+    context.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
+    const blob = await new Promise((resolve) =>
+      canvas.toBlob(resolve, "image/jpeg")
+    );
+    const arrayBuffer = await new Promise((resolve) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result);
+      fileReader.readAsArrayBuffer(blob);
+    });
+    return arrayBuffer;
+  }
+}
+
+export class BarcodeScanner {
+  constructor(videoElement, codeType) {
+    this.videoElement = videoElement;
+    this.canvas = new OffscreenCanvas(1, 1);
+    this.ctx = this.canvas.getContext("2d");
+    this.barcodeDetector = new BarcodeDetector({ formats: [codeType] });
+    this.running = false;
+    this.onScan = function (code) {
+      console.log("Detected barcode: " + code);
+    };
+  }
+
+  start() {
+    if (this.running) {
+      return;
+    }
+    this.running = true;
+    requestAnimationFrame(this.captureFrame.bind(this));
+  }
+
+  stop() {
+    this.running = false;
+  }
+
+  captureFrame() {
+    if (!this.running) {
+      return;
+    }
+    const videoWidth = this.videoElement.videoWidth;
+    const videoHeight = this.videoElement.videoHeight;
+    this.canvas.width = videoWidth;
+    this.canvas.height = videoHeight;
+    this.ctx.drawImage(this.videoElement, 0, 0, videoWidth, videoHeight);
+    const imageData = this.ctx.getImageData(0, 0, videoWidth, videoHeight);
+    this.barcodeDetector
+      .detect(imageData)
+      .then((barcodes) => {
+        if (this.running && barcodes.length > 0) {
+          // Barcode detected
+          const code = barcodes[0].rawValue;
+          this.onScan(code);
+        }
+      })
+      .finally(() => {
+        requestAnimationFrame(this.captureFrame.bind(this));
+      });
+  }
+}
