d8e2421fefaec60c6a9ba35e88ec8208cb08093f
Lasse333 <reneskjodt333@gmail.com>
2023-08-21 09:20:15 +0000

    Update 3 files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,23 @@
-import App from './App.js';
-import { clearChildren, addChildren, changeStyle } from './FastHTML.js';
-import mainStyle from "../css/style.css" assert {type: "css"}
+import App from "./App.js";
+import { clearChildren, addChildren, changeStyle } from "./FastHTML.js";
+import mainStyle from "../css/defaults/style.css" assert { type: "css" };
 
 // **  IIFE: Immediately Invoked Function Expression  */
 (async function () {
+  document.adoptedStyleSheets = [mainStyle];
 
-    document.adoptedStyleSheets = [mainStyle]
+  addChildren(document.body, await App());
 
-    addChildren(document.body, await App())
-    
-    window.addEventListener('rerender', async function (e) {
-        // console.log(e.detail, window.location.pathname)
-        if (e.detail != window.location.pathname) {
-            changeStyle(document.body, {cursor: 'wait'})
-            let pre_render = await App()
-            
-            clearChildren(document.body)
-            addChildren(document.body, pre_render)
-        }
-        
-        changeStyle(document.body, {cursor: ""})
-    });
-   
+  window.addEventListener("rerender", async function (e) {
+    // console.log(e.detail, window.location.pathname)
+    if (e.detail != window.location.pathname) {
+      changeStyle(document.body, { cursor: "wait" });
+      let pre_render = await App();
 
+      clearChildren(document.body);
+      addChildren(document.body, pre_render);
+    }
 
+    changeStyle(document.body, { cursor: "" });
+  });
 })();
