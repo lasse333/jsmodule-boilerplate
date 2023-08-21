1d15b05d3d6a7d2549cb0101863bb2a83fb0de60
Lasse333 <reneskjodt333@gmail.com>
2023-08-21 09:14:59 +0000

    Update 3 files

--- assets/js/components/Formular.js
+++ assets/js/components/Formular.js
@@ -1,207 +1,259 @@
-import { addChildren, clearChildren, createElement, changeStyle } from "../FastHTML.js";
-import RichTextAreaStyleSheet from "../../css/richTextArea.css" assert {type:"css"}
+import {
+  addChildren,
+  clearChildren,
+  createElement,
+  changeStyle,
+} from "../FastHTML.js";
 
 /**
- * 
+ *
  * @param {Object} inputs
  * @returns {HTMLElement}
  */
 export default function Formular(inputs) {
+  let form = createElement("form");
+  form.richTextInputs = [];
+  form.getFormData = () => {
+    let data = new FormData(form);
 
-    let form = createElement("form")
-    form.richTextInputs = []
-    form.getFormData = () => {
-        let data = new FormData(form)
-        
-        form.richTextInputs.forEach(input => {
-            data.append(input.textArea.name, input.textArea.value)
+    form.richTextInputs.forEach((input) => {
+      data.append(input.textArea.name, input.textArea.value);
+    });
+
+    return data;
+  };
+
+  form.getJSONData = () => {
+    let data = {};
+
+    Array.from(form).forEach((input) => {
+      if (input.type == "submit") return;
+      data[input.name] = input.value;
+    });
+
+    return data;
+  };
+
+  for (let inputName in inputs) {
+    let element;
+    let { type } = inputs[inputName];
+    let name = inputName;
+
+    let attributes = {};
+    Object.assign(attributes, inputs[inputName]);
+    Object.assign(attributes, { name: name });
+    delete attributes.type;
+
+    if (type == "textarea") element = createElement(type, attributes);
+    else if (type == "html") {
+      element = createElement("div", {}, [attributes.value ?? ""]);
+
+      form.richTextInputs.push(
+        new RichTextArea(element, {
+          attributes,
         })
+      );
+    } else if (type == "submit") {
+      element = createElement("input", inputs[inputName]);
+      element.addEventListener("click", function (e) {
+        changeStyle(this, { cursor: "progress" });
+        setTimeout(function () {
+          changeStyle(element, { cursor: "" });
+        }, 1000);
+      });
+    } else if (type == "label")
+      element = createElement(
+        type,
+        Object.assign(attributes, { innerText: name })
+      );
+    else
+      element = createElement(
+        "input",
+        Object.assign(inputs[inputName], { name })
+      );
 
-        return data
-    }
+    addChildren(form, [element]);
+  }
 
-    for (let inputName in inputs) {
-        let element
-        let {type} = inputs[inputName]
-        let name = inputName
+  form.onsubmit = (e) => {
+    e.preventDefault();
+  };
 
-        let attributes = {}
-        Object.assign(attributes, inputs[inputName])
-        Object.assign(attributes, {name: name})
-        delete attributes.type
-
-        if (type == "textarea") element = createElement(type, attributes)
-        else if (type == "html") {
-
-            element = createElement("div", {}, [
-                    attributes.value ?? ""
-            ])
-            
-            form.richTextInputs.push(new RichTextArea(element,
-                {
-                    attributes
-                }    
-            ))
-
-        }
-        else if (type == "submit") element = createElement("input", inputs[inputName])
-        else if (type == "label") element = createElement(type, Object.assign(attributes, {innerText: name}))
-        else element = createElement("input", Object.assign(inputs[inputName], {name}))
-        
-        addChildren(form, [element])
-    }
-
-    form.onsubmit = (e) => {e.preventDefault()}
-
-    return form
+  return form;
 }
 
-class RichTextArea{
-    constructor(element, options) {
-        this.element = element
-        this.options = options
-        this.selection
+class RichTextArea {
+  constructor(element, options) {
+    this.element = element;
+    this.options = options;
+    this.selection;
 
-        changeStyle(this.element, RichTextAreaStyleSheet)
-        this.value = this.options.attributes.value
+    changeStyle(
+      this.element,
+      import("../../css/richTextArea.css", { assert: { type: "css" } })
+    );
+    this.value = this.options.attributes.value;
 
-        this.toolbar = createElement("div", {class: "tool-bar"}, [
-            createElement("button", {onclick: ()=> { this.setTextStyle({fontWeight: "bold"})}, innerText: "Make Bold"}),
-            createElement("button", {onclick: ()=> { this.setTextStyle({color: "#c0392b"}) }, innerText: "Make red"}),
-            createElement("button", {onclick: ()=> { this.clearStyle() }, innerText: "Clear Formatting"}),
-            createElement("button", {onclick: ()=> { this.insertList("ul") }, innerText: "Insert Unordered List"})
-        ])
+    this.toolbar = createElement("div", { class: "tool-bar" }, [
+      createElement("button", {
+        onclick: () => {
+          this.setTextStyle({ fontWeight: "bold" });
+        },
+        innerText: "Make Bold",
+      }),
+      createElement("button", {
+        onclick: () => {
+          this.setTextStyle({ color: "#c0392b" });
+        },
+        innerText: "Make red",
+      }),
+      createElement("button", {
+        onclick: () => {
+          this.clearStyle();
+        },
+        innerText: "Clear Formatting",
+      }),
+      createElement("button", {
+        onclick: () => {
+          this.insertList("ul");
+        },
+        innerText: "Insert Unordered List",
+      }),
+    ]);
 
-        this.richText = createElement("div", {class: "rich-text"}, [this.value ?? ""])
+    this.richText = createElement("div", { class: "rich-text" }, [
+      this.value ?? "",
+    ]);
 
+    this.textArea = createElement(
+      "textarea",
+      Object.assign(this.options.attributes, {
+        value: this.value,
+        class: "raw-text",
+      })
+    );
 
-        this.textArea = createElement("textarea", Object.assign(this.options.attributes, {value: this.value, class: "raw-text"}))
+    clearChildren(this.element);
 
-        clearChildren(this.element)
+    addChildren(this.element, [this.toolbar, this.richText, this.textArea]);
 
-        addChildren(this.element, [this.toolbar, this.richText, this.textArea])
+    this.richText.contentEditable = true;
+    this.richText.oninput = this.inputEvent;
+    this.richText.onkeyup = this.selectEvent;
+    this.richText.onkeydown = this.selectEvent;
+    window.addEventListener("mouseup", this.selectEvent);
+  }
 
-        this.richText.contentEditable = true
-        this.richText.oninput = this.inputEvent
-        this.richText.onkeyup = this.selectEvent
-        this.richText.onkeydown = this.selectEvent
-        window.addEventListener("mouseup", this.selectEvent)
+  inputEvent = (e) => {
+    this.value = this.richText.innerHTML;
+    this.textArea.value = this.value;
+  };
+
+  selectEvent = () => {
+    let highlighted = this.element.shadowRoot.getSelection();
+    if (highlighted.rangeCount > 0) this.selection = highlighted.getRangeAt(0);
+  };
+
+  setTextStyle = (style) => {
+    if (
+      this.selection.startContainer === this.selection.endContainer &&
+      this.selection.startOffset === 0 &&
+      this.selection.endOffset == this.selection.startContainer.length &&
+      this.selection.startContainer.parentElement.childNodes.length -
+        this.selection.startContainer.parentElement.children.length ===
+        1
+    ) {
+      changeStyle(this.selection.startContainer.parentElement, style);
+    } else {
+      let content = this.selection.cloneContents();
+
+      let wrapper = createElement("span", { style }, [content]);
+
+      this.selection.deleteContents();
+      this.selection.insertNode(wrapper);
+
+      let newSelection = window.getSelection();
+      newSelection.setBaseAndExtent(
+        wrapper,
+        0,
+        wrapper,
+        wrapper.childNodes.length
+      );
+      this.selectEvent();
     }
 
-    inputEvent = (e) => {
-        this.value = this.richText.innerHTML
-        this.textArea.value = this.value
+    this.inputEvent();
+  };
+
+  clearStyle = () => {
+    if (
+      this.selection.startContainer === this.selection.endContainer &&
+      this.selection.startOffset === 0 &&
+      this.selection.endOffset == this.selection.startContainer.length &&
+      this.selection.startContainer.parentElement.childNodes.length -
+        this.selection.startContainer.parentElement.children.length ===
+        1
+    ) {
+      if (this.selection.startContainer.parentElement.tagName === "SPAN") {
+        this.selection.startContainer.parentElement.outerHTML =
+          this.selection.startContainer.parentElement.innerHTML;
+      }
+    } else {
+      let text = this.element.shadowRoot
+        .getSelection()
+        .toString()
+        .replaceAll("\n", "<br>");
+
+      let wrapper = createElement("span", { innerHTML: text });
+
+      this.selection.deleteContents();
+      this.selection.insertNode(wrapper);
+
+      wrapper.outerHTML = wrapper.innerHTML;
     }
 
-    selectEvent = () => {
-        let highlighted = this.element.shadowRoot.getSelection()
-        if (highlighted.rangeCount > 0) this.selection = highlighted.getRangeAt(0)
-    }
+    this.inputEvent();
+  };
 
-    setTextStyle = (style) => {
+  insertList = (listtype) => {
+    let content;
 
-        if (
-            this.selection.startContainer === this.selection.endContainer 
-            && 
-            this.selection.startOffset === 0 
-            && 
-            this.selection.endOffset == this.selection.startContainer.length
-            &&
-            (this.selection.startContainer.parentElement.childNodes.length - this.selection.startContainer.parentElement.children.length) === 1
-        ) {
-
-            changeStyle(this.selection.startContainer.parentElement, style)
-
-        } else {
-
-            let content = this.selection.cloneContents()
-
-            let wrapper = createElement("span", {style}, [content])
-
-            this.selection.deleteContents()
-            this.selection.insertNode(wrapper)
-            
-            let newSelection = window.getSelection()
-            newSelection.setBaseAndExtent(wrapper, 0, wrapper, wrapper.childNodes.length)
-            this.selectEvent()
-        }
-        
-        this.inputEvent()
-    }
-
-    clearStyle = () => {
-        if (
-            this.selection.startContainer === this.selection.endContainer 
-            && 
-            this.selection.startOffset === 0 
-            && 
-            this.selection.endOffset == this.selection.startContainer.length
-            &&
-            (this.selection.startContainer.parentElement.childNodes.length - this.selection.startContainer.parentElement.children.length) === 1
-        ) {
-
-            if (this.selection.startContainer.parentElement.tagName === "SPAN") {
-                this.selection.startContainer.parentElement.outerHTML = this.selection.startContainer.parentElement.innerHTML
-            }
-
-        } else {
-            
-            let text = this.element.shadowRoot.getSelection().toString().replaceAll("\n", "<br>")
-
-            let wrapper = createElement("span", {innerHTML: text})
-            
-            this.selection.deleteContents()
-            this.selection.insertNode(wrapper)
-
-            wrapper.outerHTML = wrapper.innerHTML
-
-        }
-
-        this.inputEvent()
-
-    }
-
-    insertList = (listtype) => {
-        let content
-
-        if (
-            this.selection.startContainer === this.selection.endContainer 
-            && 
-            this.selection.startOffset === 0 
-            && 
-            this.selection.endOffset == this.selection.startContainer.length
-            &&
-            (this.selection.startContainer.parentElement.childNodes.length - this.selection.startContainer.parentElement.children.length) === 1
-        ) {
-
-            content = this.selection.startContainer.parentElement.innerHTML
-            this.selection.startContainer.parentElement.outerHTML = `
+    if (
+      this.selection.startContainer === this.selection.endContainer &&
+      this.selection.startOffset === 0 &&
+      this.selection.endOffset == this.selection.startContainer.length &&
+      this.selection.startContainer.parentElement.childNodes.length -
+        this.selection.startContainer.parentElement.children.length ===
+        1
+    ) {
+      content = this.selection.startContainer.parentElement.innerHTML;
+      this.selection.startContainer.parentElement.outerHTML = `
             <${listtype}>
                 <li>
                     ${content}
                 </li>
             </${listtype}>
-            `
+            `;
+    } else {
+      let content = this.selection.cloneContents();
 
-        } else {
+      let wrapper = createElement(listtype, {}, [
+        createElement("li", {}, [content]),
+      ]);
 
-            let content = this.selection.cloneContents()
+      this.selection.deleteContents();
+      this.selection.insertNode(wrapper);
 
-            let wrapper = createElement(listtype, {}, [
-                createElement("li", {}, [
-                    content
-                ])
-            ])
-
-            this.selection.deleteContents()
-            this.selection.insertNode(wrapper)
-            
-            let newSelection = window.getSelection()
-            newSelection.setBaseAndExtent(wrapper, 0, wrapper, wrapper.childNodes.length)
-            this.selectEvent()
-        }
-
-        this.inputEvent()
+      let newSelection = window.getSelection();
+      newSelection.setBaseAndExtent(
+        wrapper,
+        0,
+        wrapper,
+        wrapper.childNodes.length
+      );
+      this.selectEvent();
     }
-    
+
+    this.inputEvent();
+  };
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,25 @@
 import App from './App.js';
-import { clearChildren } from './FastHTML.js';
+import { clearChildren, addChildren, changeStyle } from './FastHTML.js';
 import mainStyle from "../css/style.css" assert {type: "css"}
 
 // **  IIFE: Immediately Invoked Function Expression  */
-(function () {
+(async function () {
 
     document.adoptedStyleSheets = [mainStyle]
 
-    App();
-
-    window.addEventListener('rerender', function (e) {
+    addChildren(document.body, await App())
+    
+    window.addEventListener('rerender', async function (e) {
+        // console.log(e.detail, window.location.pathname)
         if (e.detail != window.location.pathname) {
+            changeStyle(document.body, {cursor: 'wait'})
+            let pre_render = await App()
+            
             clearChildren(document.body)
-            App()
+            addChildren(document.body, pre_render)
         }
         
+        changeStyle(document.body, {cursor: ""})
     });
    
 
