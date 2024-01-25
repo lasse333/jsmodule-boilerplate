b5f882acfe25c4ab207dd81027838c46418857a7
Lasse333 <reneskjodt333@gmail.com>
2024-01-25 10:06:55 +0000

    feat: Add BinaryExample import, add BinaryBuffer class, add BinarySection class, add Byte, Short, Int, and Long classes and implement BinaryExample function

---
+++ assets/js/pages/examples/binary.js
@@ -0,0 +1,32 @@
+import { createElement } from "../../FastHTML.js";
+import BinaryBuffer, {
+  Byte,
+  Short,
+  Int,
+  Long,
+} from "../../components/BinaryBuffer.js";
+
+export default async function BinaryExample() {
+  const myData = new BinaryBuffer();
+
+  console.log(myData); // BinaryBuffer
+
+  myData.create("byte", Byte);
+  myData.create("short", Short);
+  myData.create("int", Int);
+  myData.create("long", Long);
+
+  myData.sections.byte.unsigned = 127;
+  console.log(myData.byte.signed); // 127
+  myData.sections.byte.unsigned++;
+  console.log(myData.byte.signed); // -128
+
+  myData.long.signed = -1;
+  console.log(myData.long.unsigned); // 18446744073709551615n
+
+  return [
+    createElement("header", {}, [createElement("h1", { innerText: "Binary" })]),
+    createElement("main", {}, []),
+    createElement("footer", {}, []),
+  ];
+}
