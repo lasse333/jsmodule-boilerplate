e45841ad8688dd5d48c70ee6af72b05145e2e5d0
Lasse333 <reneskjodt333@gmail.com>
2024-02-09 15:01:01 +0000

    style: Remove commented out code

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,27 +1,8 @@
-import IndexedDB from "./components/IndexedDB.js";
 import BinaryExample from "./pages/examples/binary.js";
 import CameraExample from "./pages/examples/camera.js";
 import IndexedDBExample from "./pages/examples/indexeddb.js";
 
 export default async function App() {
-  // {
-  //   let fridge = new IndexedDB("fridge", [
-  //     { storeName: "items", dataType: "object" },
-  //     { storeName: "images", dataType: "arrayBuffer" },
-  //   ]);
-  //   await fridge.open();
-
-  //   const { items, images } = fridge.tables;
-
-  //   let buffer = new Uint8Array([1, 2, 3]).buffer;
-
-  //   let myImage = await images.add(buffer);
-  //   await items.add({ image: myImage, name: "hello" });
-  //   console.log(await myImage);
-  //   console.log(await images.get(myImage));
-
-  //   console.log(fridge);
-  // }
 
   // return await CameraExample();
   // return await BinaryExample();
